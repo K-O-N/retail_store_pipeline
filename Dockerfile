7fbc262f9356d64b2194d7e2a243809f6072ccd2
K-O-N <katenwankwo6@gmail.com>
2025-03-17 22:42:35 +0000

    pipeline changes

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,8 @@
 FROM quay.io/astronomer/astro-runtime:12.7.1
+
+# install dbt into a virtual environment
+RUN python -m venv dbt_venv && source dbt_venv/bin/activate && \
+    pip install --no-cache-dir setuptools && \
+    pip install --no-cache-dir dbt-bigquery==1.5.3 && deactivate
+
+
--- dags/retail.py
+++ dags/retail.py
@@ -2,6 +2,16 @@ from airflow.decorators import dag, task
 from datetime import datetime 
 
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
+from astro.constants import FileType
+
+from include.dbt.cosmos_config import DBT_PROJECT_CONFIG, DBT_CONFIG
+from cosmos.airflow.task_group import DbtTaskGroup
+from cosmos.constants import LoadMode
+from cosmos.config import ProjectConfig, RenderConfig
 
 
 @dag(
@@ -20,5 +30,34 @@ def retail():
 	      gcp_conn_id="gcp",
 	      mime_type="text/csv",
 )
+    create_retail_dataset = BigQueryCreateEmptyDatasetOperator(
+          task_id='create_retail_dataset',
+          dataset_id='retail',
+          gcp_conn_id='gcp',
+)   
     
-retail()    
+    gcs_to_raw = aql.load_file(
+        task_id='gcs_to_raw',
+        input_file=File(
+            'gs://retail_bucket_pip/raw/online_retail.csv',
+            conn_id='gcp',
+            filetype=FileType.CSV,
+        ),
+        output_table=Table(
+            name='raw_invoices',
+            conn_id='gcp',
+            metadata=Metadata(schema='retail')
+        ),
+        use_native_support=False,
+)       
+    transform = DbtTaskGroup(
+        group_id='transform',
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS,
+            select=['path:models/transform']
+        )
+)
+
+retail()  
